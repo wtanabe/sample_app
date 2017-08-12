0ea07c279d4a38d413814b68e13f23292c275ea7
wtanabe <tibakenmatudosi@gmail.com>
2017-08-12 06:13:13 +0000

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,7 @@ module ApplicationHelper
   # ページごとの完全なタイトルを返します。
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
-    else
-      page_title + " | " + base_title
-    end
+    return base_title if page_title.empty?
+    "#{page_title} | #{base_title}"
   end
 end
